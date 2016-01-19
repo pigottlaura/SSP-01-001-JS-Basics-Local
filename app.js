e1d5378285db1d093553d19fcc429a893e08be82
Laura Pigott <pigottlaura@gmail.com>
2016-01-19 15:15:50 +0000

    Playing with variables in JS

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 console.log("Hello World");
+
+function add(a, b)
+{
+	var theAnswer = a + b;
+	console.log(a + " + " + b + " = " + theAnswer);
+	return theAnswer;
+}
+
+var ans = add(4, 3);
+
+var name = "Laura"; //String
+var age = 28; //int
+var female = true; //Boolean
+
+if(age < 30)
+{
+	console.log("You're still young");
+}
+
+var object = new Object();
+
+var otherObject = {a: 5, b: 10};
+console.log(otherObject.a);
+
+var myArray = new Array();
+
+var myOtherArray = [22, 33, 45];
+console.log(myOtherArray[0]);
